bbd24a21f99620da21cc1820467c38a580fb62cb
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-06-15 17:41:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  
+  final_result = data.each_with_object({}) do |(key, value), final_arr|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !final_arr[name]
+          final_arr[name] = {}
+        end
+        if !final_arr[name][key]
+          !final_arr[name][key] = []
+        end
+        final_arr[name][key].push(inner_key.to_s)
+      end
 end
